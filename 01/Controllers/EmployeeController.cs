80708ecff5fa482b9f445a573f908db704e7812f
ifurkanturkoglu <turkoglu.ifurkan@gmail.com>
2023-09-17 11:40:52 +0300

    Add project files.

---
+++ 01/Controllers/EmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using _01.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _01.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly NorthwindDbContext _context;
+
+        public EmployeeController(NorthwindDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Employee()
+        {
+            return View("AddEmployee");
+        }
+        [HttpPost]
+        public IActionResult AddEmployee(Employee employee)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Employees.Add(employee);
+                _context.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+            return View("AddEmployee");
+        }
+    }
+}
---
+++ 01/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using _01.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System.Diagnostics;
+
+namespace _01.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly NorthwindDbContext _context;
+
+        public HomeController(NorthwindDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var employess = _context.Employees.ToList();
+            return View(employess);
+        }
+        
+        public IActionResult Order(int id)
+        {
+            List<Order> orders = _context.Orders.Where(a=> a.EmployeeId == id).ToList();
+            return View("Order",orders);
+        }
+
+    }
+}
